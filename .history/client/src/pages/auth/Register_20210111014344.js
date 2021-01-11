9c1332c91919622f37d0df03051f5207267ec23b
Ramon Giron II <tunenin@me.com>
2021-01-11 02:21:05 -0500

    toastcontainer, firebase email auth works

---
+++ .history/client/src/pages/auth/Register_20210111014344.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import {auth} from '../../firebase'
+import {toast, ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/react-toastify.css'
+
+
+const Register = () => {
+	const [email, setEmail] = useState('');
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const config = {
+			url:'',
+			handleCodeInApp: true,
+		}
+	};
+
+
+	const registerForm = () => (
+		<form onSubmit={handleSubmit}>
+			<input
+				type='email'
+				className='form-control'
+				value={email}
+				onChange={(e) => setEmail(e.target.value)}
+				autoFocus
+			/>
+			<button type='submit' className='btn btn-raised'>
+				Register
+			</button>
+		</form>
+	);
+
+	return (
+		<div className='container p-5'>
+			<div class='row'>
+				<div class='col-md-6 offset-md-3'>
+					<h4>Register</h4>
+					{registerForm()}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Register;
---
+++ .history/client/src/pages/auth/Register_20210111020239.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { auth } from '../../firebase';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/React-Toastify.css';
+
+const Register = () => {
+	const [email, setEmail] = useState('');
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const config = {
+			url: 'http://localhost:300/register/complete',
+			handleCodeInApp: true,
+		};
+
+		await auth.sendSignInLinkToEmail(email, config);
+		toast.success(
+			`Email is sent to ${email}, Click the link to complete your registration.`,
+		);
+		//save user email in Local storageBucket
+		window.localStorage.setItem('emailForRegistration', email);
+		
+	};
+
+	const registerForm = () => (
+		<form onSubmit={handleSubmit}>
+			<input
+				type='email'
+				className='form-control'
+				value={email}
+				onChange={(e) => setEmail(e.target.value)}
+				autoFocus
+			/>
+			<button type='submit' className='btn btn-raised'>
+				Register
+			</button>
+		</form>
+	);
+
+	return (
+		<div className='container p-5'>
+			<div class='row'>
+				<div class='col-md-6 offset-md-3'>
+					<h4>Register</h4>
+					{registerForm()}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Register;
