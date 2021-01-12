5ffe330ae0e5dc2f64d9be4f932abc68f877f754
Ramon Giron II <tunenin@me.com>
2021-01-11 20:37:00 -0500

    handlesubmit email and password validator

---
+++ .history/client/src/pages/auth/RegisterComplete_20210111203443.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from 'react';
+import { auth } from '../../firebase';
+import { toast } from 'react-toastify';
+
+const RegisterComplete = (history) => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	useEffect(() => {
+		setEmail(window.localStorage.getItem('emailForRegistration'));
+	}, []);
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		//validation error
+		if (!email || !password) {
+			toast.error('Email and password is required');
+			return;
+		}
+		if (password.length < 6) {
+			toast.error('Password must be at least 6 characters');
+			return;
+		}
+		try {
+			const result = await auth.signInWithEmailLink(
+				email,
+				window.location.href,
+			);
+			if (result.user.emailVerified) {
+				// remove user email from local storage
+				window.localStorage.removeItem('emailForRegistration');
+				//get userid
+				let user = auth.currentUser;
+				// firebase handle password
+				await user.updatePassword(password);
+				//tokens
+				const idTokenResult = await user.getIdTokenResult();
+				//redux store
+
+				//redirect
+				// history.push('/');
+			}
+		} catch (error) {
+			console.log(error);
+			toast.error(error.message);
+		}
+	};
+
+	const completeRegisterForm = () => (
+		<form onSubmit={handleSubmit}>
+			<input
+				type='email'
+				className='form-control'
+				value={email}
+				disabled
+			/>
+
+			<input
+				type='password'
+				className='form-control'
+				value={password}
+				onChange={(e) => setPassword(e.target.value)}
+				autoFocus
+				placeholder='Password'
+			/>
+
+			<button type='submit' className='btn btn-raised'>
+				Complete Registration!
+			</button>
+		</form>
+	);
+
+	return (
+		<div className='container p-5'>
+			<div class='row'>
+				<div class='col-md-6 offset-md-3'>
+					<h4>Register Complete</h4>
+
+					{completeRegisterForm()}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default RegisterComplete;
